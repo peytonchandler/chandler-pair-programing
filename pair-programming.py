31581e74264dd5507ebed3eade0aa6304fcbba47
Peyton Chandler <peytonchandler@mines.edu>
2024-02-24 18:40:55 -0700

    Add notebook ready for feedback from @Nathan

--- pair-programming.py
+++ pair-programming.py
@@ -1,17 +1,19 @@
 import numpy as np
 
 def normalize(array):
-    """Parameters
-    _______
+    """
+    ___________________________________________
+    Parameters
     c : ndarrary 
-    
-    Inputs_________
-    An array
-    
-    Returns________
+    ___________________________________________
+    Inputs
+    An array that will be normalized
+    ___________________________________________
+    Returns
     Normalized array in range [0.0, 1.0]
-        """
-    assert(type(array)==np.ndarray)
+    ___________________________________________
+    """
+    assert(type(array)==np.ndarray), 'Incorrect type for array'
     Amin = np.min(array)
     Amax = np.max(array)
     return (array - Amin)/(Amax - Amin)
